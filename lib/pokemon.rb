be133c7a2eb1f75984c692435c1516ad6de8f945
paige11 <ellis.paige.v@gmail.com>
2016-08-12 17:49:42 -0400

    Added bonus #alter_hp method then commented it out so original tests would pass

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,4 +15,8 @@ class Pokemon
   def self.find(id, db)
     db.execute("SELECT * FROM pokemon WHERE id = ?", id)
   end
+
+  # def alter_hp(new_hp)
+  #   db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, @id)
+  # end
 end
